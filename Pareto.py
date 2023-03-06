909e7d436947f5e5edf605bc02967a30fe7e92a0
Giovani da Silva <geeowdk@gmail.com>
2023-03-06 14:42:09 -0300

    vector of batches

--- Pareto.py
+++ Pareto.py
@@ -7,13 +7,20 @@ from pygmo import hypervolume
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import wandb
+
+import metrics
 
 from RewardApproximator import RewardApproximator
 from NonDominatedApproximator import NonDominatedApproximator
 
+
+
 class Pareto:
-    def __init__(self,env,epsilon_start = 1.,epsilon_decay = 0.99997,epsilon_min = 0.01,gamma = 0.98,copy_every=50,ref_point = [-1,-2] ):
+    def __init__(self,env,metrs,step_start_learning = 1000,numberofeps = 1000,epsilon_start = 1.,epsilon_decay = 0.99997,epsilon_min = 0.01,gamma = 0.98,copy_every=100,ref_point = [-1,-2] ):
         
+        self.step_start_learning  = step_start_learning
+        self.metrics = metrs
         self.epsilon_start = epsilon_start
         self.epsilon_decay = epsilon_decay
         self.epsilon_min = epsilon_min
@@ -27,11 +34,15 @@ class Pareto:
         #self.non_dominated = [[[np.zeros(nO)] for _ in range(self.nA)] for _ in range(self.nS)]
         self.number_of_actions = 4
         self.number_of_states = 120
+        self.numberofeps = numberofeps
         
         self.statesActions = np.zeros((1, 120+1))
         self.statesActionsNonDominatedEstim = np.zeros((1, 120+1))
         self.gamma = gamma
         device = 'cpu'
+        
+        
+        self.polDict = np.zeros((numberofeps,4,2))
         
         # Neural Networks
         self.reward_estimator = RewardApproximator(self.number_of_states, self.number_of_actions, nO, device=device).to(device)
@@ -73,6 +84,8 @@ class Pareto:
                 
                 
                 if use_target_nd is False:
+                    
+                    
                     o_d_aprox = self.non_dominated_estimator.forward(state,action,o1)
                 else:
                     o_d_aprox = self.target_non_dominated_estimator.forward(state,action,o1)
@@ -97,6 +110,13 @@ class Pareto:
                 
 
         return q_set
+    
+    
+    
+    def q_front(self,q_set):
+    
+    
+    
     
     #todo: faz sentido? antes eu calculei para todas as açoes, aqui é só pra uma ação, então retornaria um só ponto e não usaria os samples poins
     def calculate_q_i(self,samples,state,action):
@@ -302,8 +322,246 @@ class Pareto:
         optimizer.step()
     
     def copy_to_target(self,step):
-        if  step == self.copy_every:
-            #print("copying...")
+        if  step % self.copy_every == 0:
+            
             self.target_non_dominated_estimator.load_state_dict(self.non_dominated_estimator.state_dict())
+            
+    def pareto_frontier_policy_training(self):
+        number_of_episodes = self.numberofeps
+        number_of_p_points = 30
+        number_of_objectives = 2
+        reward_min = 0.0
+        reward_max = 128
+        log = False
+        e=0
+        memory_capacity = 150
+        from ReplayMemory import ReplayMemory
+        D = ReplayMemory(memory_capacity)
+        minibatch_size = 1   #todo: isso aqui muda??
+        self.epsilon = 0
+        from metrics import metrics
+        from deepst import DeepSeaTreasure
+        env = DeepSeaTreasure()
+        metr = metrics()
+        MAX_STEPS = 200
+        print("pareto training")
+        while e < number_of_episodes:
+            state = self.initializeState()
+            
+            terminal = False
+            acumulatedRewards = [0,0]
+            
+            step = 0
+            while terminal is False and step < MAX_STEPS:
+                #env.render()
+                
+                """samples = self.sample_points(num_samples = number_of_p_points, d = number_of_objectives, low = reward_min, high = reward_max)
+                
+                
+                
+                #os q's aqui se calcuiam a partir da target??? no caso do dqn os q's futuros vem da target e os qqqqqs atuais da model, correto?
+                qset = self.calculate_q_set(samples,state)"""
+                
+                
+            
+                
+                
+
+                qset = self.polDict[e]
+                
+                qset = {i: torch.tensor(row, dtype=torch.float64) for i, row in enumerate(qset)}
+                
+                hv = self.compute_hypervolume(qset,ref_point = np.array([-30,-20]))
+                
+                action = self.e_greedy_action(hv)
+                
+                
+                
+                
+                
+                
+                
+                next_state, reward, terminal, _ = env.step(action)
+                
+                
+                
+                acumulatedRewards[0] += reward[0]
+                acumulatedRewards[1] += reward[1]
+                
+                #print(action)
+                #add transition (s, a,r,s',t) to D
+                #transition = state, action, reward, next_state, terminal
+                D.add(state, action, reward, next_state, terminal)
+                #sample minibatch
+                minibatch = D.sample(minibatch_size)
+                minibatch = minibatch[0]
+                
+                minibatch_state = minibatch[0]
+                minibatch_action = minibatch[1]
+                minibatch_reward = minibatch[2]
+                minibatch_next_state = minibatch[3]
+                minibatch_terminal = minibatch[4]
+
+                #sample pi points, todo: também estão no intervalo do primeiro objetivo??
+                samples_i = self.sample_points(num_samples = number_of_p_points, d = number_of_objectives, low = reward_min, high = reward_max)
+                
+                
+                
+                #inputPoints = inputPoints.tolist()
+                
+                
+                
+                
+                if minibatch_terminal is not True:
+                    #todo: entender essa função
+                    #pegar a função do código do PQL
+                    q_set_hat = self.calculate_q_set(samples_i,minibatch_next_state,use_target_nd = True)
+                    
+                    inputPoints = [list([val]) for val in q_set_hat.values()]
+                    
+                    
+                    
+                    inputPoints = [[(t[0][0].item()),(t[0][1].item())] for t in inputPoints]
+                    #print(inputPoints)
+                    #exit(8)
+                    
+                    
+                    
+                    
+                
+                
+                    ndPoints, dominatedPoints = self.ND(inputPoints, self.dominates)
+                    
+                    #todo: uma descida de gradiente para cada ponto?
+                    #todo: certo isso?
+                    yi = ndPoints
+                    
+                    
+                
+                    
+                else:
+                    yi = minibatch_reward
+                
+                
+                
+                #minibatch_q_set = self.calculate_q_i(samples_i,minibatch_state,minibatch_action)
+
+                #todo: arrumar isso aqui
+                
+                
+                
+                samples_i = self.sample_points(num_samples = number_of_p_points, d = number_of_objectives, low = reward_min, high = reward_max)
+                o1 = samples_i[5]
+                
+                #uodate neural networks
+                
+                self.update_non_dominated_estimator(minibatch_state,minibatch_action,o1,yi)
+                self.update_reward_estimator(minibatch_state,minibatch_action,minibatch_reward)
+                
+                #copy to target
+                self.copy_to_target(step)
+                
+                
+                    
+                step+=1
+            
+                next_state = self.flatten_observation(next_state)
+                state = next_state
+                
+                
+            episodeSteps = step   
+            self.metrics.paretor0.append(acumulatedRewards[0])
+            self.metrics.paretor1.append(acumulatedRewards[1])
+            self.metrics.paretoepisodes.append(e)
+            
+            print('Rewards pareto = ' + str(acumulatedRewards) + '| Episode steps : ' + str(episodeSteps) + '| Episode = ' + str(e) )
+            
+            
+            self.epsilon_decrease()
+            
+            e+=1
+            
+            #print("pr1")
+            #print(self.metrics.paretor0)
+            #print("pr2")
+            #print(self.metrics.paretor1)
+            
+        if log:
+            self.send_wandb_metrics()
+            data = [[x, y] for (x, y) in zip(self.metrics.paretorewards1, self.metrics.paretorewards2)]
+            table = wandb.Table(data=data, columns = ["x", "y"])
+            wandb.log({"my_custom_plot_id" : wandb.plot.scatter(table, "x", "y", title="Custom Y vs X Scatter Plot")})
+
+
+            metr.close_wandb()
+            
+        import matplotlib.pyplot as plt
+        plt.plot(self.metrics.paretor1,self.metrics.paretor0)
+        
+        plt.ylabel("Treasure Reward  " )
+        plt.xlabel("Time Penalty " )
+        
+
+        
+        
+        
+        
+               
+        plt.show()
+        #metr.plot_p_front2(self.metrics.paretor0,self.metrics.paretor1)
+            
+    def send_wandb_metrics(self):
+        for e in range(self.numberofeps):
+            print(e)
+            log_dict3 = {
+                    #"rewards0":met.rewards1[e],
+                    #"rewards1":met.rewards2[e],
+                    "paretoreward0":self.metrics.paretor0[e],
+                    "paretoreward1":self.metrics.paretor1[e],
+                    "episode":e
+
+
+
+                }
+           
+        wandb.log(log_dict3)
     
+     
+        def pareto_front(self, obs, samples, use_target_network=False):
+        # convert to float
+        # obs = obs.astype(np.float32)
+            samples = samples.astype(np.float32)
+
+            n_samples = len(samples)
+            batch_size = len(obs)
+            obs_dims = len(obs.shape) - 1
+            # obs_dims = (-1,) + tuple(obs.shape[1:])
+            # duplicate obs to match num of samples
+            obs = np.tile(obs, (n_samples,) + (1,)*obs_dims)
+            # obs = np.repeat(obs[None, :], n_samples, axis=0).reshape(*obs_dims)
+            # assume batch of observations, duplicate samples for each obs
+            samples = np.repeat(samples, batch_size, axis=0)
+            # obs_samples = np.concatenate((obs, samples), axis=1)
+            # duplicate for each action
+            a_obs = np.tile(obs, (self.env.nA,) + (1,)*obs_dims)
+            # a_obs = np.repeat(obs[None, :], self.env.nA, axis=0).reshape(*obs_dims)
+
+            a_samples = np.tile(samples, (self.env.nA, 1))
+            # a_samples = np.repeat(samples[None, :], self.env.nA, axis=0).reshape(-1, 1)
+            # o_a_samples = np.tile(obs_samples, (self.env.nA, 1))
+            # corresponding action for each sample
+            as_ = np.repeat(np.arange(self.env.nA), n_samples*batch_size)
+
+            # shape [nA*nSamples*Batch 1]
+            oa_obj = self.estimate_objective(a_obs,
+                                            a_samples,
+                                            as_.astype(np.long),
+                                            use_target_network=use_target_network)
+            # add predicted objective to samples
+            oa_front = np.concatenate((a_samples, oa_obj), axis=1)
+            # [nA nSamples Batch nO]
+            oa_front = oa_front.reshape(self.env.nA, n_samples, batch_size, -1)
+            # [Batch nA nSamples nO]
+            oa_front = np.moveaxis(oa_front, [0, 1, 2], [1, 2, 0])
+            return oa_front
 
--- metrics.py
+++ metrics.py
@@ -12,6 +12,9 @@ class metrics():
         self.episodes = []
         self.rewards1 = []
         self.rewards2 = []
+        self.paretoepisodes = []
+        self.paretorewards1 = []
+        self.paretorewards2 = []
         self.paretor0 = []
         self.paretor1 = []
         self.nonDominatedPoints = []
@@ -27,6 +30,8 @@ class metrics():
         self.yA3 = []
         self.count = 0
         self.path = ''
+        self.writer = SummaryWriter(f"pdn")
+        
         
 
 
@@ -46,3 +51,135 @@ class metrics():
         
        
         plt.show()
+        
+    def setup_wandb(self, project_name: str, experiment_name: str):
+        self.experiment_name = experiment_name
+        import wandb
+
+        wandb.init(
+            project=project_name,
+            sync_tensorboard=True,
+            config=self.get_config(),
+            name=self.experiment_name,
+            monitor_gym=True,
+            save_code=True,
+
+        )
+        self.writer = SummaryWriter(f"{self.experiment_name}")
+        # The default "step" of wandb is not the actual time step (gloabl_step) of the MDP
+        wandb.define_metric("*", step_metric="global_step")
+
+    def close_wandb(self):
+        import wandb
+        self.writer.close()
+        wandb.finish()
+        
+    def get_config(self) -> dict:
+        """Generates dictionary of the algorithm parameters configuration
+        Returns:
+            dict: Config
+        """
+        
+        
+    def plot_p_front2(self,Xs,Ys):
+        import numpy as np
+        """
+        sorted_list = sorted([[Xs[i], Ys[i],Zs[i]] for i in range(len(Xs))], reverse=maxX)
+        
+        pareto_front = [sorted_list[0]]
+        
+        
+        for pair in sorted_list[1:]:
+            #pareto_front.append(pair)
+            if maxY:
+               
+                if pair[1] >= pareto_front[-1][1]:
+                    
+                    pareto_front.append(pair)
+            else:
+                if pair[1] <= pareto_front[-1][1]:
+                    pareto_front.append(pair)
+     
+        
+        print(self.pdict)
+        """
+        frontier = []
+        
+        Xs = self.paretor0
+        Ys = self.paretor1
+        points = np.column_stack((Xs, Ys))
+        uniques = np.unique(points,axis=0)
+        
+        inputPoints = uniques.tolist()
+        paretoPoints, dominatedPoints = simple_cull(inputPoints, dominates)
+
+        print ("*"*8 + " non-dominated answers " + ("*"*8))
+        for p in paretoPoints:
+            frontier.append(p)
+            print (p)
+        print ("*"*8 + " dominated answers " + ("*"*8))
+        for p in dominatedPoints:
+            pass
+            #print (p)
+        #print(arr)
+
+        print(frontier)
+        
+  
+        pf_Xx = [pair[0] for pair in frontier]
+        pf_Yy = [pair[1] for pair in frontier]
+          
+
+        
+      
+        
+        
+        Xs = points[:,0]
+        Ys = points[:,1]
+        
+
+        
+        plt.plot(Ys,Xs)
+        plt.scatter(pf_Yy,pf_Xx)
+        plt.xlabel("Treasure Reward  " )
+        plt.ylabel("Time Penalty " )
+        
+
+        
+        
+        
+        
+               
+        plt.show()
+        
+def simple_cull(inputPoints, dominates): 
+    paretoPoints = set()
+    candidateRowNr = 0
+    dominatedPoints = set()
+    while True:
+        candidateRow = inputPoints[candidateRowNr]
+        inputPoints.remove(candidateRow)
+        rowNr = 0
+        nonDominated = True
+        while len(inputPoints) != 0 and rowNr < len(inputPoints):
+            row = inputPoints[rowNr]
+            if dominates(candidateRow, row):
+                # If it is worse on all features remove the row from the array
+                inputPoints.remove(row)
+                dominatedPoints.add(tuple(row))
+            elif dominates(row, candidateRow):
+                nonDominated = False
+                dominatedPoints.add(tuple(candidateRow))
+                rowNr += 1
+            else:
+                rowNr += 1
+
+        if nonDominated:
+            # add the non-dominated point to the Pareto frontier
+            paretoPoints.add(tuple(candidateRow))
+
+        if len(inputPoints) == 0:
+            break
+    return paretoPoints, dominatedPoints
+def dominates(row, candidateRow):
+    return sum([row[x] >= candidateRow[x] for x in range(len(row))]) == len(row) 
